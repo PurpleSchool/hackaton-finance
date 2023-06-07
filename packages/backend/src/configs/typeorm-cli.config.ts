c0959877184a10296ab35d630c33778cd4b7d334
vb <vovakuzmin@gmail.com>
2023-06-07 22:08:11 +0300

    add migrations

---
+++ packages/backend/src/configs/typeorm-cli.config.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+export default new DataSource({
+  type: 'postgres',
+  database: configService.get('DB_NAME'),
+  host: configService.get('DB_HOST'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  port: Number(configService.get('DB_PORT')),
+  logging: true,
+  entities: [],
+  migrations: [],
+});
