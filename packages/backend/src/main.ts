5bdd952cb52fb362126be6cf42ba8635deb1a8e9
Yan Bortsov <unicag38@gmail.com>
2023-06-13 19:20:50 +0300

    remove @nestjs/swagger

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,19 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ZodValidationPipe, patchNestJsSwagger } from 'nestjs-zod';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ZodValidationPipe());
-
-  patchNestJsSwagger();
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('finance-hackaton')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 }
